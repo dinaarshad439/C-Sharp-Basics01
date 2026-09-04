42e2de16964536f68dbb1203cbd52fdde501ffd9
dinaarshad439 <dinaarshad439@gmail.com>
2026-09-04 23:01:43 +0300

    Question 1 answer

---
+++ C#Basics01/Book.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace C_Basics01
+{
+    
+    internal class Book
+    {
+        public string Title;
+
+        public int Pages;
+
+        public override string ToString()
+        {
+            return $"Title = {Title} , Pages = {Pages} ";
+        }
+    }
+}
--- C#Basics01/Program.cs
+++ C#Basics01/Program.cs
@@ -1,11 +1,26 @@
-﻿namespace C_Basics01
+﻿using System.Xml.Serialization;
+
+namespace C_Basics01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
-            
+
+            #region (Q1) created Book object from class Book and store it in variable type object
+
+            Book b = new Book();
+            Console.WriteLine("Enter Book title : ");
+            b.Title = Console.ReadLine();
+            Console.WriteLine("Enter Book Pages : ");
+            int.TryParse(Console.ReadLine(), out int num);
+            b.Pages = num;
+            object obj = b;
+            Console.WriteLine(obj);
+
+            #endregion
+
         }
     }
 }
